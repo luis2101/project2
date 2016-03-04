a19299ca990a78383f7707c727276f3f15f730d7
luis2101 <luis21012009@gmail.com>
2016-03-04 12:25:42 -0600

    Added files via upload

---
+++ Chess.java
@@ -0,0 +1,11 @@
+public class Chess
+{
+    public static void main(String[] args)
+    {
+        Board board = new Board();
+        board.print();
+        System.out.println();
+        //board.addRandomPiece();
+        board.print();
+    }
+}
